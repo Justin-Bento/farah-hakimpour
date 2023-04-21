d8fccae76268823523ceb6c0138d3a088759cdc1
Justin Bento <bento.a.justin@gmail.com>
2023-04-21 14:30:17 -0400

    created a contact page with defualt infromation

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,20 +1,178 @@
-import Appbar from '@/components/Appbar'
-import Footer from '@/components/Footer'
-import Head from 'next/head'
+import Appbar from "@/components/Appbar";
+import Footer from "@/components/Footer";
+import { BsBuilding, BsEnvelope, BsPhone } from "react-icons/bs";
 
 export default function Contact() {
   return (
     <>
-      <Head>
-        <title>Contact - Farah Hakimpour</title>
-      </Head>
-      <Appbar />
-      <main className="">
-        <section className="wrapper">
-          <h1 className="">Contact</h1>
-        </section>
+    <Appbar  />
+      <main className="relative isolate">
+        <div className="grid grid-cols-1 wrapper lg:grid-cols-2">
+          <div className="relative pt-24 pb-20 sm:pt-32 lg:static lg:py-48">
+            <div className="max-w-xl mx-auto lg:mx-0 lg:max-w-lg">
+              <h2 className="text-3xl font-bold tracking-tight text-primary-900">
+                Get in touch
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-primary-950">
+                Proin volutpat consequat porttitor cras nullam gravida at. Orci
+                molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
+                Arcu sed malesuada et magna.
+              </p>
+              <dl className="mt-10 space-y-4 text-base leading-7 text-primary-950">
+                <div className="flex gap-x-4">
+                  <dt className="flex-none">
+                    <span className="sr-only">Address</span>
+                    <BsBuilding
+                      className="w-6 text-primary-700 h-7"
+                      aria-hidden="true"
+                    />
+                  </dt>
+                  <dd>545 Mavis Island Chicago, IL 99191</dd>
+                </div>
+                <div className="flex gap-x-4">
+                  <dt className="flex-none">
+                    <span className="sr-only">Telephone</span>
+                    <BsPhone
+                      className="w-6 text-primary-700 h-7"
+                      aria-hidden="true"
+                    />
+                  </dt>
+                  <dd>
+                    <a
+                      className="hover:text-primary-900"
+                      href="tel:+1 (555) 234-5678"
+                    >
+                      +1 (555) 234-5678
+                    </a>
+                  </dd>
+                </div>
+                <div className="flex gap-x-4">
+                  <dt className="flex-none">
+                    <span className="sr-only">Email</span>
+                    <BsEnvelope
+                      className="w-6 text-primary-700 h-7"
+                      aria-hidden="true"
+                    />
+                  </dt>
+                  <dd>
+                    <a
+                      className="hover:text-primary-900"
+                      href="mailto:hello@example.com"
+                    >
+                      hello@example.com
+                    </a>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <form
+            action="#"
+            method="POST"
+            className="px-6 pt-20 pb-24 sm:pb-32 lg:px-8 lg:py-48"
+          >
+            <div className="max-w-xl mx-auto lg:mr-0 lg:max-w-lg">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-semibold leading-6 text-primary-900"
+                  >
+                    First name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-primary-900 shadow-sm ring-1 ring-inset ring-primary-300 placeholder:text-primary-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-semibold leading-6 text-primary-900"
+                  >
+                    Last name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-primary-900 shadow-sm ring-1 ring-inset ring-primary-300 placeholder:text-primary-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-semibold leading-6 text-primary-900"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-primary-900 shadow-sm ring-1 ring-inset ring-primary-300 placeholder:text-primary-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="phone-number"
+                    className="block text-sm font-semibold leading-6 text-primary-900"
+                  >
+                    Phone number
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="tel"
+                      name="phone-number"
+                      id="phone-number"
+                      autoComplete="tel"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-primary-900 shadow-sm ring-1 ring-inset ring-primary-300 placeholder:text-primary-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-semibold leading-6 text-primary-900"
+                  >
+                    Message
+                  </label>
+                  <div className="mt-2.5">
+                    <textarea
+                      name="message"
+                      id="message"
+                      rows={4}
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-primary-900 shadow-sm ring-1 ring-inset ring-primary-300 placeholder:text-primary-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-end mt-8">
+                <button
+                  type="submit"
+                  className="rounded-md bg-primary-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+                >
+                  Send message
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </main>
-      <Footer />
+    <Footer  />
+
     </>
-  )
+  );
 }
