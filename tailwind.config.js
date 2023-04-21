3246cd770ddd69f4b6c46f5c04a3dc26f5911f81
Justin Bento <bento.a.justin@gmail.com>
2023-04-21 11:36:16 -0400

    only setup sans-serif font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Raleway", "sans-serif"],
+    },
     extend: {
       colors: {
         primary: {
