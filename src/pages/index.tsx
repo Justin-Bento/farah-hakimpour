956d3f820ed82ac88865b440a0a4077a46c05c79
Justin Bento <bento.a.justin@gmail.com>
2023-04-21 12:38:34 -0400

    inclded conten tfor main image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Appbar from '@/components/Appbar'
 import Footer from '@/components/Footer'
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function index() {
   return (
@@ -9,9 +10,21 @@ export default function index() {
         <title>Home - Farah Hakimpour</title>
       </Head>
       <Appbar />
-      <main className="">
-        <section className="wrapper">
-          <h1 className="">Home</h1>
+      <main className="flex flex-col items-center lg:gap-24 lg:flex-row wrapper">
+        <section className="w-full h-48 lg:w-[570px] lg:h-[90vh] relative">
+          <Image fill quality={70} src="/media/main photo.png" className="object-cover object-center" alt="" />
+        </section>
+        <section className="flex flex-col gap-2 my-32">
+          <h1 className="text-4xl font-bold lg:text-7xl">Farah Hakimpour</h1>
+          <p className="text-base leading-10 tracking-wider lg:text-lg">See my designs, art, and love of the colour purple!</p>
+          <div className="mt-8">
+            <button
+              type="button"
+              className="px-3 py-2 text-sm font-semibold text-white rounded-md bg-primary-600 hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+            >
+              View Work
+            </button>
+          </div>
         </section>
       </main>
       <Footer />
