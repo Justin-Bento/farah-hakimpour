3d2bcdcad0fb7ad43a9ba3f3318a87973cad0805
Justin Bento <bento.a.justin@gmail.com>
2023-04-21 11:36:52 -0400

    setup the default font family to a sans serfi tailwind font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +8,7 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ["Raleway", "sans-serif"],
+      sans: ["Raleway", ...defaultTheme.fontFamily.sans],
     },
     extend: {
       colors: {
