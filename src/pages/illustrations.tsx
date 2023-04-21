f2414eef243b362ebe0b575ab57814427079e849
Justin Bento <bento.a.justin@gmail.com>
2023-04-21 12:27:39 -0400

    updated illustrations page

--- src/pages/illustrations.tsx
+++ src/pages/illustrations.tsx
@@ -2,16 +2,16 @@ import Appbar from '@/components/Appbar'
 import Footer from '@/components/Footer'
 import Head from 'next/head'
 
-export default function Illustration() {
+export default function illustrations() {
   return (
     <>
       <Head>
-        <title>Illustration - Farah Hakimpour</title>
+        <title>Illustrations - Farah Hakimpour</title>
       </Head>
       <Appbar />
       <main className="">
         <section className="wrapper">
-          <h1 className="">Illustration</h1>
+          <h1 className="">Illustrations</h1>
         </section>
       </main>
       <Footer />
