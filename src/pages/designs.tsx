c15ba4b93c419da6fcbdf41789e26795c28c8e3c
Justin Bento <bento.a.justin@gmail.com>
2023-04-21 21:58:26 -0400

    replaced dummy content from sanity

--- src/pages/designs.tsx
+++ src/pages/designs.tsx
@@ -3,25 +3,38 @@ import Footer from "@/components/Footer";
 
 import Head from "next/head";
 import Link from "next/link";
+import { client, urlFor } from "../../sanityClient";
+import Image from "next/image";
 
-export default function design() {
+export async function getStaticProps() {
+  const posts = await client.fetch(` *[_type == "post"] { _id, title, description, body, slug, "mainImage": mainImage.asset->url } `);
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function design({posts}: any) {
   return (
     <>
       <Head>
         <title>Design - Farah Hakimpour</title>
       </Head>
       <Appbar />
-      <main className="my-32 space-y-16 wrapper">
+      <main className="p-4 my-32 space-y-16 wrapper">
         <section className="space-y-2 ">
-          <h1 className="text-4xl font-extrabold leading-[1.3] tracking-[0.02rem]">Design Projects!</h1>
-          <p className="max-w-5xl text-base leading-8 tracking-wider">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptatum ducimus omnis corporis quis eveniet magni necessitatibus, ullam culpa aut, quibusdam et in quos cumque architecto ea! Accusantium, inventore enim.</p>
+          <h1 className="text-4xl font-extrabold leading-[1.3] tracking-[0.02rem]">Visually Stunning Designs From Start To Finish!</h1>
+          <p className="max-w-5xl text-base leading-8 tracking-wider">Browse through my work to get a sense of my style and the type of design solutions I can provide for your business or personal brand. Contact me to discuss your project and let's bring your vision to life together.</p>
         </section>
         <section className="grid grid-cols-1 gap-16 lg:grid-cols-3">
-          {Design_items.map((project) => {
+          {posts.map((project: any) => {
             return (
-              <Link key={project.id} href={`/designs/${project.id}`}>
-                <div  className="overflow-hidden h-[350px] transition-all border rounded-lg border-primary-300 bg-primary-200 hover:opacity-80 hover:cursor-pointer">
-                  <div className="px-4 py-5 sm:p-6">&nbsp;</div>
+              <Link key={project._id} href={`/designs/${project.slug.current}`}>
+                <div  className="overflow-hidden transition-all border rounded-lg border-primary-300 bg-primary-200 hover:opacity-80 hover:cursor-pointer">
+                  <div className="relative h-[350px]">
+                    <Image fill quality={70} src={urlFor(project.mainImage).url()} alt={project.title} className="object-cover object-center rounded-lg" />
+                  </div>
                 </div>
               </Link>
             )
@@ -32,14 +45,3 @@ export default function design() {
     </>
   );
 }
-const Design_items = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 },
-  { id: 4 },
-  { id: 5 },
-  { id: 6 },
-  { id: 7 },
-  { id: 8 },
-  { id: 9 },
-]
