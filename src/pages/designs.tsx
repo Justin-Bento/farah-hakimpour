8f21c1918ab032ae6d6b1d8111adf1aa18c05ad6
Justin Bento <bento.a.justin@gmail.com>
2023-04-21 22:56:30 -0400

    setup designs with sanity

--- src/pages/designs.tsx
+++ src/pages/designs.tsx
@@ -16,6 +16,7 @@ export async function getStaticProps() {
 }
 
 export default function design({posts}: any) {
+  
   return (
     <>
       <Head>
@@ -25,7 +26,7 @@ export default function design({posts}: any) {
       <main className="p-4 my-32 space-y-16 wrapper">
         <section className="space-y-2 ">
           <h1 className="text-4xl font-extrabold leading-[1.3] tracking-[0.02rem]">Visually Stunning Designs From Start To Finish!</h1>
-          <p className="max-w-5xl text-base leading-8 tracking-wider">Browse through my work to get a sense of my style and the type of design solutions I can provide for your business or personal brand. Contact me to discuss your project and let's bring your vision to life together.</p>
+          <p className="max-w-5xl text-base leading-8 tracking-wider">Browse through my work to get a sense of my style and the type of design solutions I can provide for your business or personal brand. Contact me to discuss your project and let&#39;s bring your vision to life together.</p>
         </section>
         <section className="grid grid-cols-1 gap-16 lg:grid-cols-3">
           {posts.map((project: any) => {
