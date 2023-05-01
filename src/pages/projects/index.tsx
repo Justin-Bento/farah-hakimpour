737332613ceade634333c997a4dc914233cefe01
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-01 11:46:29 +0100

    Everytime a catrgory is made a new sublink will be made under projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,22 +1,59 @@
-import Appbar from "@/components/Appbar"
+import { GetStaticProps } from "next";
+import { client } from "../../../sanityClient";
+import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
 import Link from "next/link";
 
-export default function ProjectsIndex() {
+interface Category {
+  _id: string;
+  title: string;
+}
+
+interface CategoryProps {
+  data: Category[];
+}
+
+export default function CategoryPage(props: CategoryProps) {
+  const { data } = props;
   return (
     <>
       <Appbar />
-      <main className="mb-32 wrapper">
+      <main className="my-32 wrapper">
         <section className="my-16 space-y-2">
-          <h1 className="font-bold display-small">Project Index Page</h1>
-          <p className="tracking-[0.015rem] body-large">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem pariatur, repudiandae quis esse eaque nesciunt quae debitis odit, recusandae facilis similique possimus tenetur optio maiores qui. Maxime ex ipsa quos?</p>
+          <h1 className="font-bold headline-large">
+            🎨🖌️ Check Out My Latest Projects!
+          </h1>
+          <p className="tracking-[0.015rem] body-large">
+            Over the my carrer, I&#39;ve been working on a variety of projects
+            for clients in different industries. I&#39;ve had the opportunity to
+            flex my creative muscles and bring my clients&#39; visions to life
+            each post is breaken down into categorys to show my versatility as
+            multimedia designer.
+          </p>
         </section>
-        <section className="grid grid-cols-1 gap-8 md:grid-cols-2" aria-label="project-category">
-          <Link href="/projects/designs" className="w-full transition-all bg-gray-300 hover:cursor-pointer hover:bg-gray-300/60 h-96" aria-label="design-project-category">&nbsp;</Link>
-          <Link href="/projects/illustrations" className="w-full transition-all bg-gray-300 hover:cursor-pointer hover:bg-gray-300/60 h-96" aria-label="illustrations-project-category">&nbsp;</Link>
-        </section>
+        <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {data.map((category) => (
+            <>
+              <Link href={`projects/${category.title.toString().toLowerCase()}`}>
+                <li key={category._id} className="overflow-hidden transition-all hover:opacity-70 bg-fuchsia-300/40 sm:rounded-lg">
+                  <div className="px-4 py-5 sm:p-6">{category.title}</div>
+                </li>
+              </Link>
+            </>
+          ))}
+        </ul>
       </main>
+
       <Footer />
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async () => {
+  const data = await client.fetch(`
+    *[_type == "category"] {_id, title}
+  `);
+  return {
+    props: { data },
+  };
+};
