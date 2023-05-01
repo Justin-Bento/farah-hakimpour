d266650c2aea73eb9cfda4ad2cf2f143014fc697
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-01 08:00:02 +0100

    created an outline for projects section

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,9 +6,10 @@ export default function ProjectsIndex() {
   return (
     <>
       <Appbar />
-      <main className="wrapper">
-        <section className="">
-          <h1 className="">Project Index Page</h1>
+      <main className="mb-32 wrapper">
+        <section className="my-16 space-y-2">
+          <h1 className="font-bold display-small">Project Index Page</h1>
+          <p className="tracking-[0.015rem] body-large">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem pariatur, repudiandae quis esse eaque nesciunt quae debitis odit, recusandae facilis similique possimus tenetur optio maiores qui. Maxime ex ipsa quos?</p>
         </section>
         <section className="grid grid-cols-1 gap-8 md:grid-cols-2" aria-label="project-category">
           <Link href="/projects/designs" className="w-full transition-all bg-gray-300 hover:cursor-pointer hover:bg-gray-300/60 h-96" aria-label="design-project-category">&nbsp;</Link>
