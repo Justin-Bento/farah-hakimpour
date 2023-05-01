d262cd57f022f1dc57c94233464bebd6affeb099
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-01 13:29:44 +0100

    updated project page to load various displines of design

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,9 +1,34 @@
-import { GetStaticProps } from "next";
-import { client } from "../../../sanityClient";
 import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
-import Link from "next/link";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { client } from "../../../sanityClient";
+import Link from "next/link";
+
+export default function projects(props: CategoryProps) {
+  const { data } = props;
+  return (
+    <>
+      <Head>
+        <title>Projects - Farah</title>
+      </Head>
+      <Appbar />
+      <main className="my-32 wrapper">
+        <section className="">
+          <h1 className="headline-large">projects</h1>
+        </section>
+        <ul className="grid grid-cols-1 gap-8 my-8 md:grid-cols-2">
+          {data.map((category) => (
+            <Link href={`/projects/${category.title.toString().toLowerCase()}`} key={category._id} className="overflow-hidden border rounded-lg hover:opacity-80 border-fuchsia-300 bg-fuchsia-200">
+              <div className="px-4 py-5 sm:p-6">{category.title}</div>
+            </Link>
+          ))}
+        </ul>
+      </main>
+      <Footer />
+    </>
+  )
+}
 
 interface Category {
   _id: string;
@@ -12,45 +37,6 @@ interface Category {
 
 interface CategoryProps {
   data: Category[];
-}
-
-export default function CategoryPage(props: CategoryProps) {
-  const { data } = props;
-  return (
-    <>
-      <Head>
-        <title>Projects - Farah Hakimpour</title>
-      </Head>
-      <Appbar />
-      <main className="my-32 wrapper">
-        <section className="my-16 space-y-2">
-          <h1 className="font-bold headline-large">
-            🎨🖌️ Check Out My Latest Projects!
-          </h1>
-          <p className="tracking-[0.015rem] body-large">
-            Over the my carrer, I&#39;ve been working on a variety of projects
-            for clients in different industries. I&#39;ve had the opportunity to
-            flex my creative muscles and bring my clients&#39; visions to life
-            each post is breaken down into categorys to show my versatility as
-            multimedia designer.
-          </p>
-        </section>
-        <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {data.map((category) => (
-            <>
-              <Link href={`projects/${category.title.toString().toLowerCase()}`}>
-                <li key={category._id} className="overflow-hidden transition-all hover:opacity-70 bg-fuchsia-300/40 sm:rounded-lg">
-                  <div className="px-4 py-5 sm:p-6">{category.title}</div>
-                </li>
-              </Link>
-            </>
-          ))}
-        </ul>
-      </main>
-
-      <Footer />
-    </>
-  );
 }
 
 export const getStaticProps: GetStaticProps<CategoryProps> = async () => {
