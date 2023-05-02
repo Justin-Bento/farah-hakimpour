daa2ca4bea791f03e7a28c126b7a021e9f4648f7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-02 17:57:12 +0100

    idk

--- src/pages/projects/[category]/index.tsx
+++ src/pages/projects/[category]/index.tsx
@@ -1,35 +1,115 @@
 import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
-import Head from "next/head";
-import Link from "next/link";
+import PageHeader from "@/components/PageHeader";
+import Error from "next/error";
 import { useRouter } from "next/router";
+import { CategoryItemComponentProps } from "../../../../typings"
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Category() {
+export default function CategoryPage() {
   const router = useRouter();
   const { category } = router.query;
-  
   return (
     <>
-      <Head>
-        <title>Category - Farah Hakimpour</title>
-      </Head>
       <Appbar />
-      <main className="p-4 my-32 space-y-8 wrapper sm:p-0">
-        <section className="space-y-2" >
-          <p className="uppercase body-small" aria-label="project-category">{category}</p>
-          <h1 className="tracking-wide headline-large" aria-label="project-category-headline">Lorem Ispum</h1>
-          <p className="tracking-wide body-large" aria-label="project-category-body">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-        </section>
-        <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <Link href={`/projects/${category}/projectName`} className="w-full transition-all rounded h-96 bg-fuchsia-200 hover:scale-95 hover:opacity-80 hover:cursor-pointer"></Link>
-          <li className="w-full transition-all rounded h-96 bg-fuchsia-200 hover:scale-95 hover:opacity-80 hover:cursor-pointer"></li>
-          <li className="w-full transition-all rounded h-96 bg-fuchsia-200 hover:scale-95 hover:opacity-80 hover:cursor-pointer"></li>
-          <li className="w-full transition-all rounded h-96 bg-fuchsia-200 hover:scale-95 hover:opacity-80 hover:cursor-pointer"></li>
-          <li className="w-full transition-all rounded h-96 bg-fuchsia-200 hover:scale-95 hover:opacity-80 hover:cursor-pointer"></li>
-          <li className="w-full transition-all rounded h-96 bg-fuchsia-200 hover:scale-95 hover:opacity-80 hover:cursor-pointer"></li>
-        </ul>
-      </main>
+      <CategoryHeader Category={category} />
       <Footer />
     </>
   )
 }
+
+function CategoryHeader({ Category }: CategoryItemComponentProps) {
+  switch (Category) {
+    case 'designs':
+    case 'illustrations':
+      return (
+        <>
+          <main className="p-4 my-16 space-y-16 wrapper sm:p-0">
+            <PageHeader Category={Category} Headline={`Sally ${Category} Packing List`} Body="" />
+            <GridLists />
+          </main>
+        </>
+      );
+    default:
+      return <Error statusCode={504} />;
+  }
+}
+
+function GridLists() {
+  const router = useRouter();
+  const { category } = router.query;
+  return (
+    <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+      {files.map((file, index) => (
+        <Link href={`/projects/${category}/${file.title.toString().toLowerCase()}`} key={index}>
+          <li className="relative">
+            <div className="block w-full overflow-hidden bg-gray-100 rounded-lg group focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+              <div className="relative aspect-video">
+                <Image fill quality={90} src={file.source} alt="" className="object-cover pointer-events-none group-hover:opacity-75" />
+              </div>
+              <button type="button" className="absolute inset-0 focus:outline-none">
+                <span className="sr-only">View details for {file.title}</span>
+              </button>
+            </div>
+            <p className="block mt-2 text-sm font-medium text-gray-900 truncate pointer-events-none">{file.title}</p>
+            <p className="block text-sm font-medium text-gray-500 pointer-events-none">{file.size}</p>
+          </li>
+        </Link>
+      ))}
+    </ul>
+  )
+}
+
+const files = [
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+  {
+    title: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+  },
+
+  // More files...
+]
