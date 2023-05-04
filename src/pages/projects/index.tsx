c31f43f67360de1568f0773594aca6a1da9efd05
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-04 23:20:40 +0100

    removed grid list

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -4,6 +4,7 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { client } from "../../../sanityClient";
 import Link from "next/link";
+import GridList from "@/components/GridList";
 
 interface Category {
   _id: string;
@@ -37,17 +38,20 @@ export default function projects(props: CategoryProps) {
           <h1 className="headline-large">Let&#39;s Create Something Beautiful Together</h1>
           <p className="body-large">As a budding graphic designer, I7&#39;m eager to explore all the design disciplines out there. From logo design to typography to web design, I7&#39;m ready to dive in and bring your vision to life. Let7&#39;s work together to create something beautiful and impactful</p>
         </section>
-        <ul className="grid grid-cols-1 gap-8 md:grid-cols-2">
-          {data.map((category) => (
-            <Link
-              href={`/projects/${category.title.toString().toLowerCase()}`}
-              key={category._id}
-              className="h-[350px] bg-fuchsia-400 hover:opacity-80 transition-all hover:cursor-pointer rounded  flex place-content-center place-items-center"
-            >
-              <div className="px-4 py-5 sm:p-6">{category.title}</div>
-            </Link>
-          ))}
-        </ul>
+          <GridList Columns="grid-cols-1 md:grid-cols-2">
+            {data.map((category) => (
+              <>
+                <Link
+                  href={`/projects/${category.title.toString().toLowerCase()}`}
+                  key={category._id}
+                  className="h-[350px] bg-fuchsia-400 hover:opacity-80 transition-all hover:cursor-pointer rounded  flex place-content-center place-items-center"
+                >
+                  <div className="px-4 py-5 sm:p-6">{category.title}</div>
+                </Link>
+              </>
+
+            ))}
+          </GridList>
       </main>
       <Footer />
     </>
