5bd66f83126002cfca7ac64ef91f8c327ba5e071
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-05 00:39:31 +0100

    created an image card

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -4,26 +4,48 @@ import Link from "next/link";
 type Props = {
   Media: string,
   Title: string,
-  Links: string
+  Links: string,
+  CardType: any
 }
 
-export default function ImageCard({ Media, Title, Links }: Props) {
-  return (
-    <>
-      <Link
-        href={Links}
-        className="flex flex-col gap-3 transition-all hover:opacity-90"
-      >
-        <div className="w-full h-[20rem] relative">
-          <Image
-            fill
-            src={Media}
-            alt=""
-            className="object-cover object-center transition-all rounded hover:grayscale hover:object-top"
-          />
-        </div>
-        <p className="body-large">{Title}</p>
-      </Link>
-    </>
-  )
+export default function ImageCard({ Media, Title, Links, CardType }: Props) {
+  if (CardType == true)  {
+    return (
+      <>
+        <Link
+          href={Links}
+          className="flex flex-col gap-3 transition-all hover:opacity-90"
+        >
+          <div className="w-full h-[20rem] relative">
+            <Image
+              fill
+              src={Media}
+              alt=""
+              className="object-cover object-center transition-all rounded hover:grayscale hover:object-top"
+            />
+          </div>
+        </Link>
+      </>
+      )
+  } else {
+    return (
+      <>
+        <Link
+          href={Links}
+          className="flex flex-col gap-3 transition-all hover:opacity-90"
+        >
+          <div className="w-full h-[20rem] relative">
+            <Image
+              fill
+              src={Media}
+              alt=""
+              className="object-cover object-center transition-all rounded hover:grayscale hover:object-top"
+            />
+          </div>
+          <p className="body-large">{Title}</p>
+        </Link>
+      </>
+    )
+  }
+
 }
