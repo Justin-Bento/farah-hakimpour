47eac160ea82ebfac3b99030df3c90d0732b2d68
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-05 00:27:00 +0100

    created dummy data for react project

--- lib/data.tsx
+++ lib/data.tsx
@@ -51,3 +51,11 @@ export const files = [
   // More files...
 ]
 
+export const data = [
+  { id: 1, link: "/#", headline: "Project Name $01", image: "https://images.unsplash.com/photo-1682496693547-77bab4b1f94e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" },
+  { id: 2, link: "/#", headline: "Project Name $01", image: "https://images.unsplash.com/photo-1682496693547-77bab4b1f94e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" },
+  { id: 3, link: "/#", headline: "Project Name $01", image: "https://images.unsplash.com/photo-1682496693547-77bab4b1f94e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" },
+  { id: 4, link: "/#", headline: "Project Name $01", image: "https://images.unsplash.com/photo-1682496693547-77bab4b1f94e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" },
+  { id: 5, link: "/#", headline: "Project Name $01", image: "https://images.unsplash.com/photo-1682496693547-77bab4b1f94e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" },
+  { id: 6, link: "/#", headline: "Project Name $01", image: "https://images.unsplash.com/photo-1682496693547-77bab4b1f94e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80" },
+]
