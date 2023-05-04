3ea45a626a8ef036a799b0d106027ab207fc5695
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-04 23:38:04 +0100

    moved code

--- src/pages/projects/[category]/index.tsx
+++ src/pages/projects/[category]/index.tsx
@@ -1,11 +1,12 @@
 import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
-import PageHeader from "@/components/PageHeader";
 import Error from "next/error";
 import { useRouter } from "next/router";
-import { CategoryItemComponentProps } from "../../../../typings"
+import { CategoryItemComponentProps } from "../../../../typings";
+import GridList from "@/components/GridList";
 import Image from "next/image";
 import Link from "next/link";
+import { files } from "../../../../lib/data";
 
 export default function CategoryPage() {
   const router = useRouter();
@@ -16,18 +17,38 @@ export default function CategoryPage() {
       <CategoryHeader Category={category} />
       <Footer />
     </>
-  )
+  );
 }
 
 function CategoryHeader({ Category }: CategoryItemComponentProps) {
   switch (Category) {
-    case 'designs':
-    case 'illustrations':
+    case "designs":
+    case "illustrations":
       return (
         <>
-          <main className="p-4 my-16 space-y-16 wrapper sm:p-0">
-            <PageHeader Category={Category} Headline={`Sally ${Category} Packing List`} Body="" />
-            <GridLists />
+          <main className="grid-cols-1 p-4 my-16 space-y-16 wrapper sm:p-0 ">
+            <GridList Columns="grid-cols-1 md:grid-cols-2">
+              {files.map((data: any) => {
+                return (
+                  <>
+                    <Link
+                      href={`/projects/${Category}/project_name`}
+                      className="flex flex-col gap-3"
+                    >
+                      <div className="w-full h-[21.875rem] relative">
+                        <Image
+                          fill
+                          src={data.source}
+                          alt=""
+                          className="object-cover object-center rounded"
+                        />
+                      </div>
+                      <p className="body-large">{data.title}</p>
+                    </Link>
+                  </>
+                );
+              })}
+            </GridList>
           </main>
         </>
       );
@@ -35,81 +56,3 @@ function CategoryHeader({ Category }: CategoryItemComponentProps) {
       return <Error statusCode={504} />;
   }
 }
-
-function GridLists() {
-  const router = useRouter();
-  const { category } = router.query;
-  return (
-    <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-      {files.map((file, index) => (
-        <Link href={`/projects/${category}/${file.title.toString().toLowerCase()}`} key={index}>
-          <li className="relative">
-            <div className="block w-full overflow-hidden bg-gray-100 rounded-lg group focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-              <div className="relative aspect-video">
-                <Image fill quality={90} src={file.source} alt="" className="object-cover pointer-events-none group-hover:opacity-75" />
-              </div>
-              <button type="button" className="absolute inset-0 focus:outline-none">
-                <span className="sr-only">View details for {file.title}</span>
-              </button>
-            </div>
-            <p className="block mt-2 text-sm font-medium text-gray-900 truncate pointer-events-none">{file.title}</p>
-            <p className="block text-sm font-medium text-gray-500 pointer-events-none">{file.size}</p>
-          </li>
-        </Link>
-      ))}
-    </ul>
-  )
-}
-
-const files = [
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'IMG_4985.HEIC',
-    size: '3.9 MB',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-
-  // More files...
-]
