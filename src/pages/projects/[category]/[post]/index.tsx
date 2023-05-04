54c60b8f06ec8d60254ed4e85edf518862eef11a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-05 00:50:03 +0100

    updated version that incorporates some best practices for React, Typescript, and Next.js

--- src/pages/projects/[category]/[post]/index.tsx
+++ src/pages/projects/[category]/[post]/index.tsx
@@ -1,17 +1,46 @@
 import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
+import Error from "next/error";
+import { FunctionComponent } from "react";
 
-export default function ProjectPost() {
+type ProjectPostProps = {
+  post: string;
+};
+
+const ProjectPost: FunctionComponent<ProjectPostProps> = ({ post }) => {
   return (
     <>
-    <Appbar />
-    <main className="p-4 my-32 wrapper sm:p-0">
-      <section className="space-y-3">
-        <h1 className="headline-large">Lorem ipsum dolor sit amet.</h1>
-        <p className="body-large">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur fuga libero aliquid facilis, voluptatem consequuntur quaerat odio ex corporis asperiores distinctio amet neque odit quos deleniti ab nemo praesentium nesciunt.</p>
-      </section>
-    </main>
-    <Footer />
+      <Appbar />
+      <main className="p-4 my-32 wrapper sm:p-0">
+        <section className="space-y-3">
+          <h1 className="headline-large">
+            {post.split("-").join(" ")}
+          </h1>
+          <p className="body-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Consequatur fuga libero aliquid facilis, voluptatem consequuntur
+            quaerat odio ex corporis asperiores distinctio amet neque odit quos
+            deleniti ab nemo praesentium nesciunt.
+          </p>
+        </section>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
+
+export const getServerSideProps = async ({ query }: any) => {
+  const { post } = query;
+  if (typeof post !== "string") {
+    return {
+      notFound: <Error statusCode={404} />,
+    };
+  }
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export default ProjectPost
