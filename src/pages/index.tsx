b6b6bd1fccf73b01fe516b87630f58e3b8e6b104
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-05 11:13:17 +0100

    fixed alingment and responsive issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,40 +2,34 @@ import Appbar from "@/components/Appbar";
 import Footer from "@/components/Footer";
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function index() {
+  const router = useRouter();
   return (
     <>
       <Head>
         <title>Home - Farah Hakimpour</title>
       </Head>
       <Appbar />
-      <main className="p-4 my-16 lg:wrapper md:p-0 md:my-0 theme">
-        <div className="flex flex-col-reverse justify-center w-full gap-8 md:justify-normal md:p-0 md:flex-row md:items-center">
-          <section className="w-full aspect-video md:w-[350px] lg:w-[570px] md:h-[93vh] relative mt-16 md:mt-0">
-            <Image
-              fill
-              quality={70}
-              src="/media/main photo.png"
-              className="object-cover object-center"
-              alt=""
-            />
-          </section>
-          <section className="flex flex-col">
-            <h1 className="text-4xl font-bold lg:text-7xl">Farah Hakimpour</h1>
-            <p className="text-base leading-10 tracking-wider lg:text-lg">
-              See my designs, art, and love of the colour purple!
-            </p>
-            <div className="mt-8">
-              <Link
-                href="/projects"
-                className="px-3 py-2 text-sm font-semibold text-white rounded-md bg-fuchsia-600 hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-fuchsia-600"
+      <main className="p-4 my-16 md:p-0 md:my-0 theme">
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="flex items-center">
+            <div className="lg:pl-24">
+              <h1 className="font-bold display-medium">Farah Hakimpour</h1>
+              <p className="mt-2 text-xl tracking-wide">See my stunning visuals that blend traditional and modern techniques and include my love of the colour purple!</p>
+              <button
+                type="button"
+                onClick={() => router.push('/projects')}
+                className="px-3 py-2 mt-8 text-sm font-semibold text-white rounded-md shadow-sm bg-fuchsia-600 hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                View Work
-              </Link>
+                View Projects
+              </button>
             </div>
-          </section>
+          </div>
+          <div className="relative w-full h-screen">
+            <Image fill quality={90} src={`/media/main photo.png`} alt="&nbsp;" className="object-cover object-center rounded-l aspect-square" />
+          </div>
         </div>
       </main>
       <Footer />
