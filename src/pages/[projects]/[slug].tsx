6b85d30f77bfd71bdc162c5623b248239120e75f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-05 11:23:21 +0100

    created a dynamic page for posts

---
+++ src/pages/[projects]/[slug].tsx
@@ -0,0 +1,12 @@
+import Appbar from "@/components/Appbar";
+import Footer from "@/components/Footer";
+export default function ProjectsSlug() {
+  return (
+    <>
+    <Appbar />
+      <main className="h-screen p-4 wrapper sm:p-0">
+      </main>
+    <Footer />
+    </>
+  )
+}
