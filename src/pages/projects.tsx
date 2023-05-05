0fc480dbd59423ab606c359481d593c3fcb6fce3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-05 10:30:59 +0100

    moved projects page outside of folder for file structure reasons

---
+++ src/pages/projects.tsx
@@ -0,0 +1,39 @@
+import Appbar from '@/components/Appbar'
+import Footer from '@/components/Footer'
+import { GetStaticProps } from 'next';
+import React from 'react'
+import { AllPosts } from '../../typings';
+import { client, urlFor } from '../../sanityClient';
+import ImageCard from '@/components/ImageCard';
+import GridList from '@/components/GridList';
+
+export default function projects({ posts }: AllPosts) {
+  console.log(posts);
+  return (
+    <>
+      <Appbar />
+      <main className="my-16 wrapper sm:p-0">
+        <section className="space-y-1.5">
+          <p className="body-small">Main Page</p>
+          <h1 className="headline-large">Here is where all projects will be listed</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ipsa, possimus repellat voluptatem ad molestias eius iste obcaecati. Optio ut eligendi ipsam eos aliquam magni! Explicabo voluptatum fugiat rem dolor!</p>
+        </section>
+        <GridList Columns="grid-cols-1 md:grid-cols-2" className='my-8'>
+          {posts.map((post: any) => {
+            return <ImageCard key={post._id} Links={post.title} Media={urlFor(post.mainImage)} Title={post.title} CardType={false} />
+          })}
+        </GridList>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps<AllPosts> = async () => {
+  const data = await client.fetch(`
+    *[_type == "post"]{_id, title,   "mainImage": mainImage.asset->urls  }
+  `);
+  return {
+    props: { posts: data },
+  };
+};
