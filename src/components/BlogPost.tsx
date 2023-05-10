0310de75c2775ccc4691e8df68b120210892eedd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-05-10 20:21:27 +0100

    included space between article

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -8,12 +8,14 @@ export default function BlogPost(props: any) {
       <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
-            <div className="lg:max-w-lg">
+            <div className="space-y-4 prose lg:max-w-lg">
               <h1 aria-label="Headline-blog-post" className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{props.Headline}</h1>
               <p aria-label="Description-blog-post" className="mt-6 text-xl ">
                 {props.Description}
               </p>
-              {<PortableText value={props.Content} />}
+              <div aria-label="content-blog-post">
+                {<PortableText value={props.Content} />}
+              </div>
             </div>
           </div>
         </div>
