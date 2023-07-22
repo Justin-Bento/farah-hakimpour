f35d3a2f8d2a33809efa2d87453ec94c733f422b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-22 19:26:08 -0400

    formatted header component

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import Logo from "@/components/Logo"
-import { Button } from "@/components/Button"
+import React, { useState } from "react";
+import Logo from "@/components/Logo";
+import { Button } from "@/components/Button";
 import { RiMenuLine, RiExternalLinkLine, RiCloseLine } from "react-icons/ri";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Appbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(true);
@@ -12,35 +12,63 @@ export default function Appbar() {
       <div className="grid grid-cols-1 gap-6 justify-evenly lg:container lg:mx-auto lg:items-center lg:grid-cols-3">
         <div className="flex justify-between">
           <Logo classNames="fill-primary-700 dark:fill-primary-50" />
-          {mobileMenuOpen ?
-            <Button variant="outline" size="icon" className="lg:hidden" onClick={onClick}>
+          {mobileMenuOpen ? (
+            <Button
+              variant="outline"
+              size="icon"
+              className="lg:hidden"
+              onClick={onClick}
+            >
               <RiMenuLine />
             </Button>
-            :
-            <Button variant="outline" size="icon" className="lg:hidden" onClick={onClick}>
+          ) : (
+            <Button
+              variant="outline"
+              size="icon"
+              className="lg:hidden"
+              onClick={onClick}
+            >
               <RiCloseLine />
             </Button>
-          }
+          )}
         </div>
         <div className={`${mobileMenuOpen ? "hidden lg:flex" : ""}`}>
           <nav className="flex flex-col justify-start w-full gap-6 lg:justify-center lg:flex-row">
             <Link href="/">
-              <Button variant="navLink" size="lg" className="w-full">Home</Button>
+              <Button variant="navLink" size="lg" className="w-full">
+                Home
+              </Button>
             </Link>
             <Link href="/about">
-              <Button variant="navLink" size="lg" className="w-full">About</Button>
+              <Button variant="navLink" size="lg" className="w-full">
+                About
+              </Button>
             </Link>
             <Link href="/projects">
-              <Button variant="navLink" size="lg" className="w-full">Projects</Button>
+              <Button variant="navLink" size="lg" className="w-full">
+                Projects
+              </Button>
             </Link>
             <Link href="/contact">
-              <Button variant="navLink" size="lg" className="w-full">Contact</Button>
+              <Button variant="navLink" size="lg" className="w-full">
+                Contact
+              </Button>
             </Link>
           </nav>
         </div>
-        <div className={`${mobileMenuOpen ? "hidden" : ""} lg:flex lg:items-center md:justify-end`}>
-          <Link target="_blank" href="https://www.linkedin.com/in/farah-hakimpour-0b41911b8/">
-            <Button variant="secondary" className="flex gap-1.5 w-full lg:w-auto">
+        <div
+          className={`${
+            mobileMenuOpen ? "hidden" : ""
+          } lg:flex lg:items-center md:justify-end`}
+        >
+          <Link
+            target="_blank"
+            href="https://www.linkedin.com/in/farah-hakimpour-0b41911b8/"
+          >
+            <Button
+              variant="secondary"
+              className="flex gap-1.5 w-full lg:w-auto"
+            >
               <RiExternalLinkLine />
               LinkedIn
             </Button>
@@ -48,5 +76,5 @@ export default function Appbar() {
         </div>
       </div>
     </header>
-  )
+  );
 }
