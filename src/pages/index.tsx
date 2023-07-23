26526a454b3ac57da68f8ed839379c43d8a83023
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-22 20:35:51 -0400

    addedd link to projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer'
 import { Button } from '@/components/Button'
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function index() {
   return (
@@ -11,12 +12,14 @@ export default function index() {
       <main className="p-4 py-12 md:p-0 lg:container lg:mx-auto lg:p-0">
         <div className="flex flex-col items-start gap-8 md:flex-row md:justify-between md:items-center">
           <div className="">
-            <h1 className="text-5xl font-extrabold tracking-tight scroll-m-20 text-primary-960 ">Farah Hakimpour</h1>
+            <h1 className="text-5xl font-bold tracking-tight scroll-m-20 text-primary-960 ">Hi &#128075;, I&#39;m Farah Hakimpour!</h1>
             <p className="mt-3 text-xl leading-7 tracking-wide">See my designs, art, and love of the colour purple!</p>
-            <Button className="mt-6">Continue</Button>
+            <Link href="/projects">
+              <Button variant="secondary" className="mt-6">Continue</Button>
+            </Link>
           </div>
           <div className="relative w-full aspect-square md:h-screen md:w-72 lg:w-96">
-            <Image fill alt='' src="/media/main photo.png" className="object-cover rounded md:rounded-none" />
+            <Image fill alt='' src="/media/main photo.png" className="object-cover rounded md:rounded-none " />
           </div>
         </div>
       </main>
