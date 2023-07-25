ebb1ccb7d3e1174fe6c57c4c9462d2ad02c83227
Justin Bento <bento.a.justin@gmail.com>
2023-07-25 14:02:34 -0400

    updated font wegit spacing and aligment

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -17,8 +17,8 @@ export default function projects({ posts }: AllPosts) {
       <Appbar />
       <main className="p-4 my-16 lg:container lg:mx-auto sm:p-0">
         <section className="space-y-1.5">
-          <h1 className="pb-2 text-3xl font-semibold tracking-tight capitalize transition-colors scroll-m-20 first:mt-0">Here is where all projects will be listed</h1>
-          <p className="text-base lg:text-xl text-primary-960/70">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ipsa, possimus repellat voluptatem ad molestias eius iste obcaecati. Optio ut eligendi ipsam eos aliquam magni! Explicabo voluptatum fugiat rem dolor!</p>
+          <h1 className="pb-2 text-3xl font-extrabold tracking-normal capitalize transition-colors scroll-m-20 first:mt-0">Here is where all projects will be listed</h1>
+          <p className="text-base tracking-wide lg:text-xl text-primary-960/70">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ipsa, possimus repellat voluptatem ad molestias eius iste obcaecati. Optio ut eligendi ipsam eos aliquam magni! Explicabo voluptatum fugiat rem dolor!</p>
         </section>
         <GridList Columns="grid-cols-1 md:grid-cols-2" className='my-8'>
           {posts.map((post: any) => {
