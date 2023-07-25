783320f803331484bf87bde5fd5ab6d179234ae1
Justin Bento <bento.a.justin@gmail.com>
2023-07-25 14:02:50 -0400

    updated font wegit spacing and aligment

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -16,7 +16,7 @@ export default function ProjectsSlug({ post }: any) {
       <main className="p-4 my-10 md:my-16 lg:container lg:mx-auto lg:p-0 lg:max-w-[80ch]">
         <article>
           <section className="space-y-4">
-            <h1 className="text-4xl font-bold tracking-normal scroll-m-20">{post.title}</h1>
+            <h1 className="text-4xl font-extrabold tracking-wide scroll-m-20">{post.title}</h1>
             <p className="text-xl text-primary-960/80">{post.description}</p>
             <div className="relative aspect-video ">
               <Image fill quality={40} src={urlFor(post.mainImage).url()} alt={`Image of ${post.title}`} className="object-cover " />
