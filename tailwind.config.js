236b10947abcab4b49a90dca50d445b85b94ba16
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-27 11:18:01 -0400

    updated extend fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Wix Madefor Text", ...defaultTheme.fontFamily.sans],
+        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: {
