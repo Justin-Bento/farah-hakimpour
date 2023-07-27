6369eb40d8f7aa3f7600d44cbab4eeacec7c0e46
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-27 11:11:42 -0400

    fixed html removed global components

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -14,8 +14,7 @@ export default function projects({ posts }: AllPosts) {
       <Head>
         <title>Projects - Farah Hakimpour</title>
       </Head>
-      <Appbar />
-      <main className="p-4 my-16 lg:container lg:mx-auto sm:p-0">
+      <article className="p-4 my-16 lg:container lg:mx-auto sm:p-0">
         <section className="space-y-1.5">
           <h1 className="pb-2 text-3xl font-extrabold tracking-normal capitalize transition-colors scroll-m-20 first:mt-0">Here is where all projects will be listed</h1>
           <p className="text-base tracking-wide lg:text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ipsa, possimus repellat voluptatem ad molestias eius iste obcaecati. Optio ut eligendi ipsam eos aliquam magni! Explicabo voluptatum fugiat rem dolor!</p>
@@ -25,8 +24,7 @@ export default function projects({ posts }: AllPosts) {
             return <ImageCard key={post._id} Links={`/projects/${post.slug.current}`} Media={urlFor(post.mainImage).url()} Title={post.title} CardType={false} />
           })}
         </GridList>
-      </main>
-      <Footer />
+      </article>
     </>
   )
 }
