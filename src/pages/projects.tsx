299722cd79eb7b44d8846810988397ba9eb177bd
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 12:38:32 -0400

    fixed content for projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -16,8 +16,8 @@ export default function projects({ posts }: AllPosts) {
       </Head>
       <article className="p-4 my-16 lg:container lg:mx-auto sm:p-0">
         <section className="space-y-1.5">
-          <h1 className="pb-2 text-3xl font-extrabold tracking-normal capitalize transition-colors scroll-m-20 first:mt-0">Here is where all projects will be listed</h1>
-          <p className="text-base tracking-wide lg:text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ipsa, possimus repellat voluptatem ad molestias eius iste obcaecati. Optio ut eligendi ipsam eos aliquam magni! Explicabo voluptatum fugiat rem dolor!</p>
+          <h1 className="pb-2 text-5xl font-extrabold tracking-normal capitalize transition-colors scroll-m-20 first:mt-0">Dive into My Creative Process!</h1>
+          <p className="text-base tracking-wide lg:tracking-wider lg:text-xl">Welcome to my world of art and imagination! Here, you&#39;ll find a collection of wonderful projects that showcase my unique artistic style. From beautiful illustrations to stunning graphic designs, each creation is a reflection of my deep passion for creativity.</p>
         </section>
         <GridList Columns="grid-cols-1 md:grid-cols-2" className='my-8'>
           {posts.map((post: any) => {
