1b4ef035e68ee176f4d4295941978f5583853ee5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-09-02 16:03:36 -0400

    setup breadcrumb for the about page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/Button";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function About() {
   return (
@@ -40,6 +42,15 @@ export default function About() {
           <Image fill src="/media/about-page.webp" alt="picture of farah" className="object-cover" />
         </div>
         <article className=" max-w-[90ch] mx-auto my-8 lg:my-16 p-4">
+        <nav className="flex items-center gap-2">
+          <Link href='/'>
+            <Button variant="ghost" size="sm" className="dark:text-primary-200/80 hover:underline">Home</Button>
+          </Link>
+          <span className="dark:text-primary-200/50">/</span>
+          <Link href='/projects'>
+            <Button variant="ghost" size="sm" className="dark:text-primary-200/80 hover:underline">About</Button>
+          </Link>
+        </nav>
           <h1 className="text-4xl font-extrabold lg:leading-tight tracking-normal scroll-m-20 [text-wrap:balance] capitalize lg:text-5xl">Farah Hakimpour, an artist who specializes in digital art, photography, and graphic design</h1>
           <div className="my-5 space-y-5 text-lg leading-7 tracking-wide">
             <p>Hi there, I&#39;m Farah Hakimpour - a Graphic Designer and Self Taught Artist. I&#39;m all about creating cool stuff that makes people go &#34;wow!&#34; Whether it&#39;s a branding project, website mockup, or fan art piece, I put my heart and soul into everything I create.</p>
