f25225077a0df6875d99c270eb3a1f70e9a277eb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-09-13 10:02:41 -0400

    added more content to slug.current

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -15,6 +15,8 @@ export default function ProjectsSlug({ post }: any) {
       </Head>
       <article className="p-4 py-12 md:py-16 lg:container lg:mx-auto lg:py-20 lg:max-w-[80ch]">
         <section className="my-8 space-y-8 tracking-wide prose-lg">
+          <h1>{post.title}</h1>
+          <p>{post.description}</p>
           <PortableText value={post.body} components={RichTextComponents} />
         </section>
       </article>
@@ -40,7 +42,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }: any) {
-  const query = `*[_type == "post" && slug.current == $slug] { body }
+  const query = `*[_type == "post" && slug.current == $slug] { title, description, body }
   `;
   const options = { slug: params.slug };
   const post = await client.fetch(query, options);
