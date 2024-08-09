2cfc4eb17917c69982a6d35a41bc902baed44ce1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-09 17:18:08 -0400

    Removed unused import from project page.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,7 +5,6 @@ import { client, urlFor } from "../../sanityClient";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
