9c5c71a83ed10289c3be09cdc6287447863d5bbd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-03 16:30:14 -0500

    Created the header component to aid users in navigatng pages.

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import React, { useState } from "react";
+import Logo from "@/components/Logo";
+import { Button } from "@/components/ui/button";
+import { RiMenuLine, RiExternalLinkLine, RiCloseLine } from "react-icons/ri";
+import Link from "next/link";
+
+export default function Navigation() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(true);
+  const onClick = () => setMobileMenuOpen((mobileMenuOpen) => !mobileMenuOpen);
+  return (
+    <header className="sticky top-0 z-10 p-4 lg:p-0 bg-white shadow shadow-black/10 dark:bg-black dark:shadow-white/10">
+      <div className="grid grid-cols-1 gap-6 justify-evenly lg:container sm:px-6 lg:px-8 lg:mx-auto lg:items-center lg:grid-cols-3">
+        <div className="flex items-center justify-between">
+          <Link href="/" aria-label="link-to-home">
+            <Logo classNames="" />
+          </Link>
+          {mobileMenuOpen ? (
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={onClick}
+              className="lg:hidden"
+              aria-label="nav-button-open"
+            >
+              <RiMenuLine />
+            </Button>
+          ) : (
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={onClick}
+              className="lg:hidden"
+              aria-label="nav-button-close"
+            >
+              <RiCloseLine />
+            </Button>
+          )}
+        </div>
+        <div className={`${mobileMenuOpen ? "hidden lg:flex" : ""}`}>
+          <nav className="flex flex-col justify-start w-full gap-6 lg:justify-center lg:flex-row">
+            <Link href="/">
+              <Button variant="link" aria-label="navigation-link-home">
+                Home
+              </Button>
+            </Link>
+            <Link href="/about">
+              <Button variant="link" aria-label="navigation-link-about">
+                About
+              </Button>
+            </Link>
+            <Link href="/projects">
+              <Button variant="link" aria-label="navigation-link-projects">
+                Projects
+              </Button>
+            </Link>
+            <Link href="/contact">
+              <Button variant="link" aria-label="navigation-link-contact">
+                Contact
+              </Button>
+            </Link>
+          </nav>
+        </div>
+        <div
+          className={`${mobileMenuOpen ? "hidden" : ""
+            } lg:flex lg:items-center md:justify-end`}
+        >
+          <Link
+            target="_blank"
+            href="https://www.linkedin.com/in/farah-hakimpour-0b41911b8/"
+          >
+            <Button
+              variant="secondary"
+              aria-label="external-link-to-linkedin"
+            >
+              <RiExternalLinkLine className="mb-1 mr-1" />
+              LinkedIn
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
