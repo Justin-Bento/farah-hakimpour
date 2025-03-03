7f7ac8ba9c23862a3369cab8c73ee11b6aa17fd7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-03 15:44:37 -0500

    Changed up the page to be a danymic card grid.

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -1,22 +1,39 @@
 import Link from "next/link";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
 
 export default async function Page() {
   const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
 
   return (
-    <main className="max-w-prose mx-auto mt-24">
+    <main className="container mx-auto mt-24">
       <h1 className="mb-4 text-3xl font-bold tracking-tight">Hello, Projects Page.</h1>
-      <ul className="grid grid-cols-1 gap-4 mb-4 divide-y divide-blue-100">
-        {posts.map((post) => (
-          <li key={post._id}>
-            <Link
-              className="block p-4 hover:text-gray-500"
-              href={`/projects/${post?.slug?.current}`}
-            >
-              {post?.title}
-            </Link>
+      <ul className="grid grid-cols-3 gap-6">
+        {posts.map((post: any) => (
+          <li>
+            <Card className="group border hover:border-blue-300">
+              <CardHeader>
+                <AspectRatio ratio={16 / 9} className="relative">
+                  <Image src={urlFor(post.mainImage)
+                    .width(2432)
+                    .height(1442)
+                    .quality(80)
+                    .auto("format")
+                    .url()}
+                    alt={post?.mainImage?.alt || ""} fill className="rounded-md" />
+                </AspectRatio>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <CardTitle>{post?.title}</CardTitle>
+                <CardDescription>{post?.description}</CardDescription>
+                <Link href="/#" className="text-muted-foreground text-sm group-hover:underline">See More</Link>
+              </CardContent>
+            </Card>
           </li>
         ))}
       </ul>
