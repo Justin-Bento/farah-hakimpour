36a3dce62bd6b19256729298a691ff5528c3a032
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-03 13:59:59 -0500

    Fetched posts created for faraths projects

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Link from "next/link";
+import { sanityFetch } from "@/sanity/lib/live";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
 
-export default function page() {
+export default async function Page() {
+  const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
+
   return (
     <main className="max-w-prose mx-auto mt-24">
-      <h1 className="text-3xl font-bold tracking-tight">Hello, Projects Page</h1>
+      <h1 className="mb-4 text-3xl font-bold tracking-tight">Hello, Projects Page.</h1>
+      <ul className="grid grid-cols-1 gap-4 mb-4 divide-y divide-blue-100">
+        {posts.map((post) => (
+          <li key={post._id}>
+            <Link
+              className="block p-4 hover:text-gray-500"
+              href={`/posts/${post?.slug?.current}`}
+            >
+              {post?.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <hr className="opacity-25 my-4" />
+      <Link href="/" className="text-sm">&larr; Return home</Link>
     </main>
-  )
+  );
 }
