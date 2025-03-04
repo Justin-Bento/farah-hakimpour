12ebab0a6bc3819c074e51d605c68fa6225e2761
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-03 22:45:13 -0500

    Updated contact page

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import Head from "next/head";
+import Link from "next/link";
+import { RiMailLine } from "react-icons/ri";
+import { Button } from '@/components/ui/button'
+import ContactForm from "@/components/ContactForm";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
-export default function page() {
+
+export default function Contact() {
   return (
-    <main className="min-h-dvh container mx-auto">
-      <h1 className="text-3xl font-bold tracking-tight">Hello, Contact Page</h1>
-    </main>
-  )
+    <>
+      <main className="container mx-auto min-h-screen mt-32">
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-x-12 lg:gap-y-24">
+          <div className="">
+            <Breadcrumb className="mb-3">
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage className="text-gray-800 underline">Contact</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">
+              Want To Get In Touch?
+            </h1>
+            <p className="text-lg text-muted-foreground my-2">
+              Enter in your information here to get in touch with me!
+            </p>
+            <hr className="border-black/30 mt-6" />
+            <Link
+              className="tracking-wide hover:text-gray-900"
+              href="mailto:farahsdesignss@gmail.com"
+            >
+              <Button variant="link" className="mt-6 hover:text-purple-700 hover:underline hover:cursor-pointer dark:text-purple-50/70">
+                <RiMailLine
+                  className="size-4"
+                  aria-hidden="true" />
+                farahsdesignss@gmail.com
+              </Button>
+            </Link>
+          </div>
+          <div className="w-full">
+            <ContactForm />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
