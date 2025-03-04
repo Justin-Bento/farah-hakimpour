6a49a5ff495a453f9559e0383b53eec07c5d202a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-04 02:28:09 -0500

    removed tepmlate litterals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${raleway}`}>
+    <html lang="en" className={raleway.className}>
       <body>
         <ThemeProvider
           attribute="class"
